f288ba951953df8e8974d7b9b59a58558b8036e5
Axel20213tn018 <20213tn018@utez.edu.mx>
2022-11-29 17:15:28 -0600

    Inicio de interfaces

--- SISGEB/src/app/app.module.ts
+++ SISGEB/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+import { AddEditUserComponent } from './components/add-edit-user/add-edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListUsersComponent,
+    AddEditUserComponent
   ],
   imports: [
     BrowserModule
