37448ab2a7de7feb88ba24f35b0d51690829737d
Axel20213tn018 <20213tn018@utez.edu.mx>
2022-12-01 15:00:04 -0600

    Lista de usuarios en contrucción

--- SISGEB/src/app/components/list-users/list-users.component.ts
+++ SISGEB/src/app/components/list-users/list-users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/interfaces/users';
 
 @Component({
   selector: 'app-list-users',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class ListUsersComponent {
 
+  listUsers: User[] = [
+    // Uso para pruebas, proximamente el arreglo se llenará con datos de la Base de Datos
+    {id: 1, name: "Axel", surname: "Hernandez", phone: "7775975695", address: "Cuernavaca", email: "20213tn018@utez.edu.mx", password:"admin12345"},
+    {id: 2, name: "Natalia", surname: "Garcia", phone: "7777856214", address: "Temixco", email: "20213tn015@utez.edu.mx", password:"nat123"},
+    {id: 3, name: "Andrea", surname: "Estrada", phone: "7778755421", address: "Cuernavaca", email: "20213tn011@utez.edu.mx", password:"mich123"},
+    {id: 4, name: "Elena", surname: "Abigail", phone: "7771234857", address: "Cuernavaca", email: "20203tn002@utez.edu.mx", password:"elena123"},
+  ]
+
+
+  constructor(){ }
+
+  ngOnInit(): void {
+
+  }
 }
